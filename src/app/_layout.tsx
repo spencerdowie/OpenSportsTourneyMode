a4e04be93eaa601608e922c653a391861db0ea3b
spencerdowie <spencer.dowie@hotmail.com>
2026-07-23 23:37:49 -0400

    [+] Added 'phone' frame around app base

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,26 @@
 import { Stack } from "expo-router";
+import { View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <View
+      style={{
+        flexGrow: 1,
+        backgroundColor: "grey",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <View
+        style={{
+          width: 402,
+          height: 874,
+          borderWidth: 3,
+          borderColor: "black",
+          borderRadius: 20,
+          overflow: "hidden"
+        }}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </View>
+    </View>
+  );
 }
