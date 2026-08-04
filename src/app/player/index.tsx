65dcb6997ad4c4cfaa3266c4ae88d74f5ee1b371
spencerdowie <spencer.dowie@hotmail.com>
2026-08-04 10:49:17 -0400

    [*] Updated player dash to grab info from mock json and register context

--- src/app/player/index.tsx
+++ src/app/player/index.tsx
@@ -1,17 +1,49 @@
+import data from "@/assets/tournament.json";
 import Button from "@/components/button";
 import MatchDetail from "@/components/match-display";
 import PageHeader from "@/components/page-header";
 import StatusBox from "@/components/status-box";
 import TitledSection from "@/components/titled-section";
-import { router } from "expo-router";
-import { useState } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+import { useContext, useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { AppContext } from "../_layout";
+
+//Mock database schema
+type TournamentData = {
+  id: number;
+  name: string;
+  organizer: { orgName: string; admin: string };
+  date: string;
+  location: string;
+  registrationOptions: { value: string; name: string }[];
+  teams: { id: number; name: string }[];
+  players: { id: number; name: string }[];
+};
 
 export default function Player() {
-  const [tournamentInfo, SetTournamentInfo] = useState({
+  const [tournamentInfo, SetTournamentInfo] = useState<TournamentData>({
+    id: -1,
     name: "Test",
-    organizer: { orgName: "Test", admin: "Test" }
+    organizer: { orgName: "Test", admin: "Test" },
+    date: "",
+    location: "",
+    registrationOptions: [],
+    teams: [{ id: -1, name: "" }],
+    players: [{ id: -1, name: "" }]
   });
+  const { tournament: id } = useLocalSearchParams<{
+    tournament?: string;
+  }>();
+  const { name, skillLevel, options } = useContext(AppContext);
+  useEffect(() => {
+    if (id) {
+      let tournament = data.find((t) => t.id == Number.parseInt(id));
+      if (tournament) {
+        SetTournamentInfo(tournament);
+      }
+    }
+  }, [tournamentInfo]);
 
   return (
     <View style={{ marginHorizontal: 24 }}>
